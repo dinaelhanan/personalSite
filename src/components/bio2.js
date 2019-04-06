4c346102b5f37089a61e9b529b60f6d74cfb1661
dina <dinaelhanan@gmail.com>
2019-04-05 21:38:19 -0400

    small fix

--- src/components/bio2.js
+++ src/components/bio2.js
@@ -7,7 +7,7 @@ import { rhythm } from "../utils/typography"
 function Bio() {
   return (
     <StaticQuery
-      query={bioQuery}
+      query={bioQuery2}
       render={data => {
         const { author, social } = data.site.siteMetadata
         return (
