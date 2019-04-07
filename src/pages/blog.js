c73fae99b2b6059237f6fbaecac8db7350712261
d1g1t4ld1n4 <dinaelhanan@gmail.com>
2019-04-06 21:56:32 -0400

    Update blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
+import Logo from "../components/logo.js"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
 class BlogIndex extends React.Component {
@@ -13,7 +14,8 @@ class BlogIndex extends React.Component {
     return (
    
              
-      <Layout location={this.props.location} title={siteTitle}>
+      <Layout location={this.props.location}>
+      <Logo />
         <SEO
           title="All posts"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
