fc6c57133839a3a39a98670f0d8e1dddc4e2212a
digitaldina <dinaelhanan@gmail.com>
2021-01-06 14:21:18 -0500

    Create bloglogo.js

---
+++ src/components/bloglogo.js
@@ -0,0 +1,13 @@
+
+import React from "react"
+import logo from "../../static/logo.png"
+class BlogLogo extends React.Component {
+  render() {
+    return (
+       <a href="http://dinaelhanan.com"> <p class="logo"  >digital<br>dina</p></a>
+     
+    )
+  }
+}
+
+export default Logo
