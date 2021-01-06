cc5ca2bd82b2c2ffc824ca727d94ac2f60af37ca
digitaldina <dinaelhanan@gmail.com>
2021-01-06 14:21:00 -0500

    Update logo.js

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,7 +1,4 @@
 
-/*kodus to this codepen for the highlighted style:
-https://codepen.io/carlitocenteno/pen/myxgod
-*/
 import React from "react"
 import logo from "../../static/logo.png"
 class Logo extends React.Component {
