92acb28b4c0b47e9ea8c850cfedc6e803ceb811f
digitaldina <dinaelhanan@gmail.com>
2021-01-07 10:48:09 -0500

    Update bloglogo.js

--- src/components/bloglogo.js
+++ src/components/bloglogo.js
@@ -4,10 +4,10 @@ import logo from "../../static/logo.png"
 class BlogLogo extends React.Component {
   render() {
     return (
-       <a href="http://dinaelhanan.com"> <p class="logo"  >digital<br>dina</p></a>
+       <a href="http://dinaelhanan.com"> <p class="logo"  >digital<br/>dina</p></a>
      
     )
   }
 }
 
-export default Logo
+export default BlogLogo
